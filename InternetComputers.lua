67fd11a1db30c282964e979677b39a1aa49f9669
ICYPhoenix7 <90792373+ICYPhoenix7@users.noreply.github.com>
2023-11-11 23:12:40 -0800

    Add more computers, add checks for script starting

--- InternetComputers.lua
+++ InternetComputers.lua
@@ -1,33 +1,53 @@
+local function StartScript(string, stack_size)
+    if not SCRIPT.DOES_SCRIPT_EXIST(string) then
+        return false
+    end
+    if SCRIPT_GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(joaat(str)) > 0 then
+        return true
+    end
+    SCRIPT.REQUEST_SCRIPT(string)
+    while not SCRIPT.HAS_SCRIPT_LOADED (string) do
+        script_util:yield()
+    end
+    SYSTEM.START_NEW_SCRIPT(str, arg_count or 0)
+    SCRIPT.SET_SCRIPT_AS_NO_LONGER_NEEDED(string)
+    return true
+end
+
 internet_computers = gui.get_tab("GUI_TAB_NETWORK"):add_tab("Internet") --Internet, lol
 
 internet_computers:add_button("Start Bunker Business Computer", function()
     script.run_in_fiber(function (script)
-		if SCRIPT.GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(joaat("appBunkerBusiness")) == 0 then
-			SYSTEM.START_NEW_SCRIPT('appBunkerBusiness', 1424)
-		end
+		StartScript('appBunkerBusiness', 1424)
     end)
 end)
 
 internet_computers:add_button("Start Business Hub Computer", function()
     script.run_in_fiber(function (script)
-		if SCRIPT.GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(joaat("appBusinessHub")) == 0 then
-			SYSTEM.START_NEW_SCRIPT('appBusinessHub', 1424)
-		end
+		StartScript('appBusinessHub', 1424)
     end)
 end)
 
 internet_computers:add_button("Start Arcade Computer", function()
     script.run_in_fiber(function (script)
-		if SCRIPT.GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(joaat("appArcadeBusiness")) == 0 then
-			SYSTEM.START_NEW_SCRIPT('appArcadeBusiness', 1424)
-		end
+		StartScript('appArcadeBusiness', 1424)
     end)
 end)
 
 internet_computers:add_button("Start Master Control Terminal", function()
     script.run_in_fiber(function (script)
-		if SCRIPT.GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(joaat("appArcadeBusinessHub")) == 0 then
-			SYSTEM.START_NEW_SCRIPT('appArcadeBusinessHub', 1424)
-		end
+		StartScript('appArcadeBusinessHub', 1424)
+    end)
+end)
+
+internet_computers:add_button("Start Terrorbyte Computer", function()
+    script.run_in_fiber(function (script)
+		StartScript('appHackerTruck', 4592)
+    end)
+end)
+
+internet_computers:add_button("Start Special Cargo Warehouse Computer", function()
+    script.run_in_fiber(function (script)
+		StartScript('appsecuroserv', 4592)
     end)
 end)
