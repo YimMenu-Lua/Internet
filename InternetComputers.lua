f37b919689ca51d080b12cdbdf1bf39ed6dee187
gir489returns <gir489returns@outlook.com>
2023-11-12 16:31:20 -0500

    Made script work consistently after using it the first time and buying something. Added MC computers.

--- InternetComputers.lua
+++ InternetComputers.lua
@@ -1,47 +1,103 @@
-local function StartScript(string, stack_size)
-    if not SCRIPT.DOES_SCRIPT_EXIST(string) then
-        return false
+has_bought_something = 1950700
+global_computer_type = 1969858
+
+function start_script(script_name, stack_size, script)
+    if SCRIPT.GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(joaat(script_name)) >= 1 then
+        return
     end
-    if SCRIPT.GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(joaat(str)) > 0 then
-        return true
+    SCRIPT.REQUEST_SCRIPT(script_name)
+    repeat script:yield() until SCRIPT.HAS_SCRIPT_LOADED(script_name)
+    SYSTEM.START_NEW_SCRIPT(script_name, stack_size)
+    SCRIPT.SET_SCRIPT_AS_NO_LONGER_NEEDED(script_name)
+    script:sleep(100)
+    if script_name == 'appArcadeBusinessHub' then
+        while SCRIPT.GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(joaat(script_name)) >= 1 do
+            if globals.get_int(global_computer_type) == -1 then
+                globals.set_int(has_bought_something, 0)
+            end
+            script:yield()
+        end
+    else
+        repeat script:yield() until SCRIPT.GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(joaat(script_name)) == 0
+        globals.set_int(has_bought_something, 0)
     end
-    SCRIPT.REQUEST_SCRIPT(string)
-    while not SCRIPT.HAS_SCRIPT_LOADED (string) do
-        script_util:yield()
-    end
-    SYSTEM.START_NEW_SCRIPT(string, arg_count or 0)
-    SCRIPT.SET_SCRIPT_AS_NO_LONGER_NEEDED(string)
-    return true
 end
 
 internet_computers = gui.get_tab("GUI_TAB_NETWORK"):add_tab("Internet") --Internet, lol
 
 internet_computers:add_button("Start Bunker Business Computer", function()
     script.run_in_fiber(function (script)
-		StartScript('appBunkerBusiness', 1424)
+        start_script('appBunkerBusiness', 1424, script)
     end)
 end)
-
+internet_computers:add_sameline()
 internet_computers:add_button("Start Business Hub Computer", function()
     script.run_in_fiber(function (script)
-		StartScript('appBusinessHub', 1424)
-    end)
-end)
-
-internet_computers:add_button("Start Arcade Computer", function()
-    script.run_in_fiber(function (script)
-		StartScript('appArcadeBusiness', 1424)
-    end)
-end)
-
-internet_computers:add_button("Start Master Control Terminal", function()
-    script.run_in_fiber(function (script)
-		StartScript('appArcadeBusinessHub', 1424)
+        start_script('appBusinessHub', 1424, script)
     end)
 end)
 
 internet_computers:add_button("Start Terrorbyte Computer", function()
     script.run_in_fiber(function (script)
-		StartScript('appHackerTruck', 4592)
+        start_script('appHackerTruck', 4592, script)
+    end)
+end)
+internet_computers:add_sameline()
+internet_computers:add_button("Start Smuggler", function()
+    script.run_in_fiber(function (script)
+        start_script('appSmuggler', 4592, script)
+    end)
+end)
+
+internet_computers:add_button("Start Internet", function()
+    script.run_in_fiber(function (script)
+        start_script('appInternet', 4592, script)
+    end)
+end)
+internet_computers:add_sameline()
+internet_computers:add_button("Start Arcade Computer", function()
+    script.run_in_fiber(function (script)
+        start_script('appArcadeBusiness', 4592, script)
+    end)
+end)
+
+internet_computers:add_button("Start Cocaine Computer", function()
+    script.run_in_fiber(function (script)
+        globals.set_int(global_computer_type, 8)
+        start_script('appBikerBusiness', 4592, script)
+    end)
+end)
+internet_computers:add_sameline()
+internet_computers:add_button("Start Meth Computer", function()
+    script.run_in_fiber(function (script)
+        globals.set_int(global_computer_type, 6)
+        start_script('appBikerBusiness', 4592, script)
+    end)
+end)
+
+internet_computers:add_button("Start Weed Computer", function()
+    script.run_in_fiber(function (script)
+        globals.set_int(global_computer_type, 7)
+        start_script('appBikerBusiness', 4592, script)
+    end)
+end)
+internet_computers:add_sameline()
+internet_computers:add_button("Start Counterfeit Cash Computer", function()
+    script.run_in_fiber(function (script)
+        globals.set_int(global_computer_type, 9)
+        start_script('appBikerBusiness', 4592, script)
+    end)
+end)
+
+internet_computers:add_button("Start Document Forgery Computer", function()
+    script.run_in_fiber(function (script)
+        globals.set_int(global_computer_type, 10)
+        start_script('appBikerBusiness', 4592, script)
+    end)
+end)
+internet_computers:add_sameline()
+internet_computers:add_button("Start Master Control Terminal", function()
+    script.run_in_fiber(function (script)
+        start_script('appArcadeBusinessHub', 1424, script)
     end)
 end)
